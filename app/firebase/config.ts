17b8c06012c0f71f8fcb18de751c9ba465d07a19
Krishan Maan <krishankumarmaan400@gmail.com>
2025-03-23 18:21:52 +0530

    fc

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyCdrU2pRVYKZynugwM6XEmpSsOGoA9s4gU",
+  authDomain: "realestate-6fc31.firebaseapp.com",
+  databaseURL: "https://realestate-6fc31-default-rtdb.firebaseio.com",
+  projectId: "realestate-6fc31",
+  storageBucket: "realestate-6fc31.firebasestorage.app",
+  messagingSenderId: "1028103624896",
+  appId: "1:1028103624896:web:7c92fda4d2c33def51f525",
+  measurementId: "G-RFTPNXMTNS"
 };
 
 // Initialize Firebase
